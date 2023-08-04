e912b6b2f36f3d7fbb74713e3a5007af8c92465d
Crowdbotics <engineering@crowdbotics.com>
2023-08-04 07:28:06 +0000

    Initial commit.

---
+++ backend/quiet_rice_dev_107094/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for quiet_rice_dev_107094 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiet_rice_dev_107094.settings')
+
+application = get_wsgi_application()
